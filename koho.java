cf60fde1d3ba8245d3018fa75ec6c62d35788501
batman1292 <batman12922@gmail.com>
2016-02-04 11:41:22 +0700

    create

---
+++ koho.java
@@ -0,0 +1,28 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+
+/**
+ *
+ * @author 2IDR-PC
+ */
+public class koho {
+    
+//    init variable 
+    static int grid_size = 10;
+    static int num_sample = 16;
+    static int num_attribute = 13;
+    static double weigth [][][] = new double [grid_size][grid_size][num_attribute];
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+        // TODO code application logic here
+    }
+    
+    public void readData(){
+        
+    }
+}
