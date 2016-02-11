126c1038b6a29af7e4dc2fad91d7a5b3bb3e6924
choocku <checkypw@gmail.com>
2016-02-11 10:10:18 +0700

    complete version

--- src/kohonen/Kohonen.java
+++ src/kohonen/Kohonen.java
@@ -20,7 +20,7 @@ public class Kohonen {
     static int grid_size = 10;
     static int num_sample = 16;
     static int num_attribute = 13;
-    static int input_vec_dim = 2;
+    static int input_vec_dim = 13;
     static double weight[][][] = new double[grid_size][grid_size][num_attribute];
     static double input[][] = new double[num_sample][num_attribute];
     static String input_name[] = new String[num_sample];
@@ -28,11 +28,13 @@ public class Kohonen {
 
     static double ETA_TIME_CONST = 4000;
     static double SIGMA_TIME_CONST = 4000;
-    static int MAX_EPOCH = 2000;
+    static int MAX_EPOCH = 20000;
     static double sigma_init = 0.9;
     static double eta_init = 0.9;
 
-    static double mse_error[] = new double[2000];
+    static double mse_error[] = new double[MAX_EPOCH];
+    
+    static int output[][] = new int[grid_size][grid_size];
 
     /**
      * @param args the command line arguments
@@ -43,7 +45,7 @@ public class Kohonen {
         random_weight();
 
         //train 
-        double distance, new_distance;
+        double distance = 0, new_distance;
 
         int num_epoch,
                 num_iteration,
@@ -54,12 +56,12 @@ public class Kohonen {
         double distance_sq_between_neuron,
                 neighbour_function;
 
-        double error_n[] = new double[num_sample];
+        double error_n[] = new double[num_sample+1];
 
         double eta, sigma;
 
         for (num_epoch = 0; num_epoch < MAX_EPOCH; ++num_epoch) {
-            for (num_iteration = 0; num_iteration < num_sample; num_iteration++) {
+            for (num_iteration = 0; num_iteration < num_sample; ++num_iteration) {
                 /* Find the nearest weight to the current training pattern */
                 double sum = 0.0;
 
@@ -70,12 +72,11 @@ public class Kohonen {
                 distance = Math.sqrt(sum);
                 nearest_weight_row_index = 0;
                 nearest_weight_column_index = 0;
-                
 
-                for (int i = 0; i < grid_size; i++) {
-                    for (int j = 0; j < grid_size; j++) {
+                for (int i = 0; i < grid_size; ++i) {
+                    for (int j = 0; j < grid_size; ++j) {
                         sum = 0.0;
-                        for (int k = 0; k < input_vec_dim; k++) {
+                        for (int k = 0; k < input_vec_dim; ++k) {
                             sum += (input[num_iteration][k] - weight[i][j][k])
                                     * (input[num_iteration][k] - weight[i][j][k]);
                         }
@@ -93,7 +94,7 @@ public class Kohonen {
                 /* Calculate error between the nearest centre and the current
                  training pattern */
                 error_n[num_iteration] = 0.0;
-                for (int i = 0; i < input_vec_dim; i++) {
+                for (int i = 0; i < input_vec_dim; ++i) {
                     error_n[num_iteration]
                             += (input[num_iteration][i] - weight[nearest_weight_row_index][nearest_weight_column_index][i])
                             * (input[num_iteration][i] - weight[nearest_weight_row_index][nearest_weight_column_index][i]);
@@ -108,7 +109,7 @@ public class Kohonen {
                 /* Adjust connection weight */
                 for (int i = 0; i < grid_size; ++i) {
                     for (int j = 0; j < grid_size; ++j) {
-
+output[i][j] = -1;
                         /* calculate distance between winning neuron and 
                          current neuron */
                         distance_sq_between_neuron
@@ -126,8 +127,8 @@ public class Kohonen {
                 }
                 iteration_counter++;
             }
-            System.out.println("distance=" + distance);
-            num_iteration--;
+//            System.out.println("distance=" + distance);
+//            num_iteration--;
 
 //            System.out.println("number of iterations = " + num_iteration);
             mse_error[num_epoch] = 0.0;
@@ -137,6 +138,7 @@ public class Kohonen {
             mse_error[num_epoch] = mse_error[num_epoch] / (double) num_sample;
         }
         System.out.println("Total number of iterations = " + iteration_counter);
+        
 //        test
         for (num_iteration = 0; num_iteration < num_sample; num_iteration++) {
             /* Find the nearest weight to the current training pattern */
@@ -151,10 +153,11 @@ public class Kohonen {
             nearest_weight_row_index = 0;
             nearest_weight_column_index = 0;
 //System.out.println("test distance=" + distance);
-            for (int i = 0; i < grid_size; i++) {
-                for (int j = 0; j < grid_size; j++) {
+            for (int i = 0; i < grid_size; ++i) {
+                for (int j = 0; j < grid_size; ++j) {
+                    
                     sum = 0.0;
-                    for (int k = 0; k < input_vec_dim; k++) {
+                    for (int k = 0; k < input_vec_dim; ++k) {
                         sum += (input[num_iteration][k] - weight[i][j][k])
                                 * (input[num_iteration][k] - weight[i][j][k]);
                     }
@@ -168,14 +171,31 @@ public class Kohonen {
                 } /* for */
 
             } /* for */
-            System.out.print("at num_iteration " + num_iteration + "\t");
-//            System.out.print("nearest_weight_row_index = " + nearest_weight_row_index + "\t");
-//            System.out.println("nearest_weight_column_index = " + nearest_weight_column_index + "\t");
+
+            System.out.print("at num_iteration " + input_name[num_iteration] + "\t");
+            System.out.print("nearest_weight_row_index = " + nearest_weight_row_index + "\t");
+            System.out.println("nearest_weight_column_index = " + nearest_weight_column_index + "\t");
+            output[nearest_weight_row_index][nearest_weight_column_index] = num_iteration;
+//            System.out.println(output[nearest_weight_row_index][nearest_weight_column_index]);
+        }
+        
+        for (int i = 0; i < grid_size; i++) {
+            System.out.print("|\t");
+            for (int j = 0; j < grid_size; j++) {
+                if(output[i][j] > -1){
+                    System.out.print(input_name[output[i][j]]+"\t");
+                }else{
+                    System.out.print("\t");
+                }
+//                 System.out.print(output[i][j]+"\t");
+                System.out.print("|\t");
+            }
+            System.out.println("");
         }
     }
 
     public static void readData() {
-        String path = "D:\\BVH\\MEE\\Intelligent System\\Task3\\koho\\src\\animal.txt";
+        String path = "D:\\anatoliy\\MEE-Term2\\Intelligence System\\Koho\\animal.txt";
         File file = new File(path);
         try {
             BufferedReader br = new BufferedReader(new FileReader(file));
